62b30937f95e2aa624ddb6d0555b46d56b3ff398
Donald-Macak <don.mack83@ymail.com>
2023-11-25 12:54:17 -0600

    changes

--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
 const { app } = require('./app');
+
+const PORT = process.env.PORT || 5000;
+
+/*
+app.listen(PORT, async () => {
+ console.log('Server started listening on port: 3000');
+ console.log(process.env.MONGO_URI);
+ try {
+  await mongoose.connect(process.env.MONGO_URI, {});
+  console.log('Connected to Mongo DB.');
+ } catch (error) {
+  console.log(error);
+ }
+});
+*/
+
+const start = async () => {
+  try {
+  } catch (err) {
+    // throw DB Conneciton Error
+  }
+};
