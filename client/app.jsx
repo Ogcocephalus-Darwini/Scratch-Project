4427d979804c899c73fd8659b222399757c4dead
John Banks <johnlouismbanks@gmail.com>
2023-11-25 15:38:23 -0500

    package json
    Co-authored-by: mckenzie-morris <mckenzie-morris@users.noreply.github.com>
    Co-authored-by: Jwong24601 <Jwong24601@users.noreply.github.com>

--- client/app.jsx
+++ client/app.jsx
@@ -2,10 +2,61 @@ import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = () => {
+  const [userNameInput, setUsername] = useState('');
+
+  const [passwordInput, setPassword] = useState('');
+
+  const handleUsernameInput = (event) => {
+    return setUsername(event.target.value);
+  };
+
+  const handlePasswordInput = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSignUpClick = () => {
+    signUP(userNameInput, passwordInput);
+  };
+
+  const handleLoginClick = () => {
+    login(userNameInput, passwordInput);
+  };
+
+  const signUP = (uName, pWord) => {
+    fetch('/', {
+      method: 'POST',
+      body: { uName, pWord },
+    });
+  };
+
+  const login = (uName, pWord) => {
+    fetch('/', {
+      method: 'GET',
+      body: { uName, pWord },
+    });
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <div>Hellow World</div>
+
+      <label>
+        Username:
+        <input onChange={handleUsernameInput} value={userNameInput} type="text" name="name" />
+      </label>
+
+      <label>
+        Password:
+        <input onChange={handlePasswordInput} value={passwordInput} type="text" name="name" />
+      </label>
+
+      <button id="signup" onClick={handleClick}>
+        Sign Up
+      </button>
+      <button id="login" onClick={handleClick}>
+        Login
+      </button>
+
       <h1> </h1>
     </div>
   );
